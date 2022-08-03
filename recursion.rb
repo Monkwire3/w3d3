ba25ccf355ecfa55d8b19447c3caa231d95a8a87
Will Pierson <willcpierson@gmail.com>
2022-08-03 13:51:17 -0400

    exponents completed, started dupe

--- recursion.rb
+++ recursion.rb
@@ -17,3 +17,25 @@ def exponentiation_1(base, xp)
     return 1 if xp == 0
     return base * exponentiation_1(base, xp - 1)
 end
+
+def exponentiation_2(base, xp)
+    return 1 if xp == 0
+    return base if xp == 1
+    if xp % 2 == 0
+        prev = exponentiation_2(base, xp / 2)
+        return prev * prev
+    else
+        prev = exponentiation_2(base, (xp - 1) / 2) * 2
+        base * prev
+    end
+end
+
+# p exponentiation_1(2, 3)
+# p exponentiation_2(2, 3)
+# p exponentiation_2(2, 4)
+
+def deep_dup(multi_array)
+    return multi_array.map { |ele| ele} if multi_array.all? { |sub_ele| !sub_ele.is_a?(Array)}
+
+
+end
