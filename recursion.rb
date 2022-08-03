5b4057a23a4ba1f261a9b0428277808ed3a6c4e3
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-08-03 12:15:19 -0400

    swap

--- recursion.rb
+++ recursion.rb
@@ -1,1 +1,19 @@
 #recursion
+
+def range_iter(start, end_1)
+    return [] if end_1 < start
+    (start...end_1).map {|el| el}
+end
+
+def recurs_range(start, ending)
+    return [] if ending <= start
+    return [start] if ending - 1 == start
+
+
+    return  [start] + recurs_range(start + 1, ending)
+end
+
+def exponentiation_1(base, xp)
+    return 1 if xp == 0
+    return base * exponentiation_1(base, xp - 1)
+end
